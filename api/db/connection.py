2f67c987de679da07f8e051ac475c941f2788ec8
stefano-chen <stefanochen01@gmail.com>
2025-10-02 10:25:41 +0200

    db connection impl

---
+++ api/db/connection.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from ..common.settings import get_settings
+
+settings = get_settings()
+
+engine = create_engine(url=f"postgresql://{settings.PSQL_USER}:{settings.PSQL_PASSWORD}@{settings.PSQL_HOST}:{settings.PSQL_PORT}/{settings.PSQL_DB}")
+
+def get_db_session():
+    with Session(bind=engine) as session:
+        try:
+            yield session
+        finally:
+            session.close()
